25a17b98a872f2e8ae33755a323f20247fa0a752
Beau Barker <beau.barker@amtek.com.au>
2015-05-20 16:27:12 +1000

    Many adjustments

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ from setuptools import setup
 
 setup(
     name='loginbp',
-    py_modules=['loginbp']
+    py_modules=['loginbp'],
+    install_requires=['flask', 'flask_assets', 'flask_login', \
+        'flask_principal', 'flask_wtf', 'wtforms', 'jsonrpcclient']
     )
