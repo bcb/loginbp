f8b0b5ae9bb6c137eb13c02181887246a26e7a9c
Beau Barker <beauinmelbourne@gmail.com>
2015-05-20 16:27:12 +1000

    Many adjustments

--- loginbp.py
+++ loginbp.py
@@ -3,18 +3,21 @@
 import logging
 from os.path import dirname
 
-from flask import Blueprint
-from flask import render_template, request, flash, redirect, url_for
-from flask_login import UserMixin, LoginManager, login_user, logout_user
-from flask_wtf import Form
+from flask import render_template, request, flash, redirect, url_for, \
+    Blueprint, current_app, session
+from flask_assets import Bundle
 from wtforms import StringField, PasswordField, validators
-from flask_assets import Environment, Bundle
+from flask_wtf import Form
+from flask_login import UserMixin, LoginManager, login_user, logout_user, \
+    login_required, current_user
+from flask_principal import Principal, Identity, AnonymousIdentity, UserNeed, \
+    RoleNeed, identity_loaded, identity_changed
 from jsonrpcclient import Server
 from jsonrpcclient.exceptions import ReceivedErrorResponse
 
 
 logger = logging.getLogger(__name__)
-loginbp = Blueprint('loginbp', __name__, template_folder='templates',
+loginbp = Blueprint('loginbp', __name__, template_folder='templates', \
     static_folder='static')
 login_manager = LoginManager()
 users_api = Server('http://localhost/api/users', auth=('admin', 'heiwah4i'))
@@ -22,20 +25,38 @@ users_api = Server('http://localhost/api/users', auth=('admin', 'heiwah4i'))
 
 @loginbp.record_once
 def on_load(state):
-    # webassets
+    """Perform blueprint actions that require the app variable"""
+    # Flask-Login
+    login_manager.init_app(state.app)
+    # Flask-Principal
+    Principal(state.app, skip_static=True)
+    @identity_loaded.connect_via(state.app)
+    def on_identity_loaded(_, identity): #pylint:disable=unused-variable
+        """Flask-Principal: Called when logging in or out"""
+        identity.user = current_user
+        # UserNeeds
+        if hasattr(current_user, 'username'):
+            logger.info('Identity loaded: %s', current_user.username)
+            identity.provides.add(UserNeed(current_user.username))
+        # RoleNeeds
+        if hasattr(current_user, 'roles'):
+            for role in current_user.roles:
+                identity.provides.add(RoleNeed(role.name))
+    # Webassets
     assets = state.app.assets
     assets.append_path(dirname(__file__)+'/static')
-    js_login = Bundle('js/jQuery-Notify-bar/jquery.notifyBar.js', 'js/login.js',
-        filters='jsmin', output='packed-login.js')
+    js_login = Bundle('js/jQuery-Notify-bar/jquery.notifyBar.js', \
+        'js/login.js', filters='jsmin', output='packed-login.js')
     assets.register('js_login', js_login)
-    css_login = Bundle('js/jQuery-Notify-bar/css/jquery.notifyBar.css',
+    css_login = Bundle('js/jQuery-Notify-bar/css/jquery.notifyBar.css', \
         'css/login.css', filters='cssmin', output='packed-login.css')
     assets.register('css_login', css_login)
-    # loginbp
-    login_manager.init_app(state.app)
 
 
 class User(UserMixin):
+    """Instantiated when login was successful, populated with data returned
+    from the api, then passed to Flask-Login methods.
+    """
     def __init__(self, user_id, username):
         self.user_id = user_id
         self.username = username
@@ -46,29 +67,30 @@ class User(UserMixin):
 
 @login_manager.user_loader
 def load_user(user_id):
-    """Load user"""
+    """Flask-Login: Load user"""
     user_dict = users_api.get_id(user_id, response=True)
     user = User(user_id, user_dict['username'])
     return user
 
 
-class LoginForm(Form):
-    """Login form"""
-    username = StringField('Username', validators=[validators.required()])
-    password = PasswordField('Password', validators=[validators.required()])
-
-
 @loginbp.route('/login', methods=['GET', 'POST'])
 def login():
-    """Login"""
+    """Login route"""
+    class LoginForm(Form):
+        """Login form"""
+        username = StringField('Username', validators=[validators.required()])
+        password = PasswordField('Password', validators=[validators.required()])
     form = LoginForm()
+    # Validate form
     if form.validate_on_submit():
         username = request.form['username']
         password = request.form['password']
+        # Attempt login via api
         try:
-            user_dict = users_api.login(username=username, password=password,
+            user_dict = users_api.login(username=username, password=password, \
                 response=True)
         except ReceivedErrorResponse as e:
+            logger.warning('Invalid username or password')
             flash('Invalid username or password')
         except Exception as e:
             logger.error(str(e))
@@ -76,16 +98,32 @@ def login():
         else:
             user = User(user_dict['id'], username)
             if login_user(user, remember=True):
+                # Flask-Principal
+                identity_changed.send(current_app._get_current_object(), \
+                    identity=Identity(user.user_id))
+                # Redirect
+                logger.info('Logged in as %s', username)
                 flash('Logged in as %s' % username)
-                return redirect(request.args.get('next') or url_for('promos'))
+                return redirect(request.args.get('next') or url_for('home'))
             else:
+                logger.error('Sorry but you could not log in')
                 flash('Sorry, but you could not log in')
     return render_template('login.html', url=url_for('.login'), form=form)
 
 
 @loginbp.route('/logout')
+@login_required
 def logout():
-    """Logout"""
+    """Logout route"""
+    # Flask-Login
     logout_user()
+    # Flask-Principal: Remove session keys
+    for key in ('identity.name', 'identity.auth_type'):
+        session.pop(key, None)
+    # Flask-Principal: Set identity to anonymous
+    identity_changed.send(current_app._get_current_object(), \
+        identity=AnonymousIdentity())
+    # Redirect
+    logger.info('Logged out')
     flash('Logged out.')
     return redirect(url_for('.login'))
